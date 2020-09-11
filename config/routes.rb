1d74444b7e5f33e578f5b01a07edb7a5ae055867
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-11 14:44:48 +0530

    Added link_to

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   get 'home/ok'
   get 'home/index'
-  get 'home/about'
-  get 'home/contact'
+  get 'home/about', as: :about   #about_path or about_url
+  get 'home/contact', as: :contact
 end
