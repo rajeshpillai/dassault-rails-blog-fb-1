1619fc595a7040ee6275c74358c73d47b1b97c36
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-11 15:33:35 +0530

    Scaffold Category

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,11 @@ class HomeController < ApplicationController
   # views/controllername/index.html.erb
 
   def ok 
-
     render plain: "OK"
   end
 
   def index 
-    
+    @posts = ["post 1", "post 2"]
   end
 
   def about 
