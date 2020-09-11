26f1579f5ec69a757a4ea217b67d681e0090d55d
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-11 17:51:56 +0530

    before_action filter

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,16 +1,22 @@
 class TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :destroy]
+  
   def index
     @tags = Tag.all 
   end
 
   def show 
-    @tag = Tag.find(params[:id])
+    # @tag = Tag.find(params[:id])
   end
 
   def destroy 
-    @tag = Tag.find(params[:id])
+    # @tag = Tag.find(params[:id])
     @tag.destroy 
     redirect_to tags_path
   end
 
+  private 
+  def set_tag 
+    @tag = Tag.find(params[:id])
+  end
 end
