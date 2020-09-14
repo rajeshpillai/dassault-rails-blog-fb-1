d65d2aca01564baece36f53903444a82f6d24aab
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-14 14:52:39 +0530

    Named and default scope added to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@
 # One post record in the the DB -> Active Record Pattern
 class Post < ApplicationRecord
   belongs_to :category
+
+  # default scope
+  default_scope { order :created_at}
+
+  scope :published, -> { where(published: true)}
+  scope :unpublished, -> { where.not(published: true)}
+
+
 end
 
 
