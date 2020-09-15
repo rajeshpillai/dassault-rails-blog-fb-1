39e89712c8213938212f0413c4c38ef0f9f76811
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-15 20:56:50 +0530

    Tag input fix with checbox using virtual attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,8 +17,25 @@ class Post < ApplicationRecord
   # Override default scope (reorder)
   scope :order_by_latest, -> { reorder(created_at: :desc)}
 
+  attr_accessor :all_tags
+  after_save :assign_tags
+
+  def assign_tags 
+    if @all_tags.blank?
+      return
+    end
+    # tag_names: => tag1, tag2, tag3
+    # NOTE: DO NOTE USE all_tags below as it will call getter
+    #       USE @all_tags
+    self.tags = @all_tags.split(",").map do |tag_name|
+       unless tag_name.blank?
+          Tag.where(name: tag_name.strip).first_or_create!
+       end
+    end
+  end
+
   # Custom/virtual methods for fields
-  def all_tags=(names)
+  def all_tagsx=(names)
     p "all_tags: #{names}"
     if names.blank?
       return
